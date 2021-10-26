1a089cdc87ab6697f0b9d9b8f34d7788b9995650
9ml <85211376@qq.com>
2021-10-26 11:50:39 +0800

    add suspense

---
+++ Web/07-Vue/v3/demo/21-src-suspense组件/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <h2>App组件</h2>
+    <Suspense>
+      <!-- 异步组件引入完成展示 -->
+      <template v-slot:default>
+        <Child />
+      </template>
+      <!-- 异步组件未引入完成时展示 -->
+      <template v-slot:fallback>
+        <h3>加载中...</h3>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script>
+  // 静态引入
+  // import Child from './components/Child'
+  // 引入定义异步组件的 API
+  import { defineAsyncComponent } from 'vue'
+  // 异步引入 动态引入
+  const Child = defineAsyncComponent(() => import('./components/Child'))
+  export default {
+    name: 'App',
+    components: {
+      Child
+    }
+  }
+</script>
+
+<style>
+  .app {
+    background: gray;
+    padding: 10px;
+  }
+</style>
---
+++ Web/07-Vue/v3/demo/21-src-suspense组件/components/Child.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="child">
+    <h2>Child组件</h2>
+    <h3>{{sum}}</h3>
+  </div>
+</template>
+
+<script>
+  import { ref } from 'vue'
+  export default {
+    name: 'Child',
+    async setup() {
+      let sum = ref(0)
+      // 模拟异步
+      let p = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({ sum })
+        }, 3000)
+      })
+      return await p
+    }
+  }
+</script>
+
+<style>
+  .child {
+    background: skyblue;
+    padding: 10px;
+  }
+</style>
